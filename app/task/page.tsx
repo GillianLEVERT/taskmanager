7d1eddb5da111157f55fa0b6aff31da77c3fed1c
SnakouZ <gillianlevert.dev@protonmail.com>
2024-06-25 17:10:47 +0200

    css 3

--- app/task/page.tsx
+++ app/task/page.tsx
@@ -3,7 +3,8 @@
 import { useState } from "react";
 import CreateTaskForm from "../../components/CreateTaskForm";
 import TaskList from "../../components/TaskList";
-import './task.css';
+import Link from "next/link";
+import "./task.css";
 
 export default function TaskPage() {
   const [refreshTrigger, setRefreshTrigger] = useState(0);
@@ -15,18 +16,20 @@ export default function TaskPage() {
   return (
     <section className="task-page">
       <div className="">
+        <h1 className="title">Gestionnaires des tâches</h1>
 
-      <h1 className="title">Gestionnaires des tâches</h1>
-
-      <div className="text">
-        <CreateTaskForm onTaskCreated={handleTaskCreated} />
-      </div>
+        <div className="text">
+          <CreateTaskForm onTaskCreated={handleTaskCreated} />
+        </div>
       </div>
 
       <div className="task-container mt-20">
         <h2 className="title">Vos tâches</h2>
         <TaskList key={refreshTrigger} />
       </div>
+      <Link href="/" className="link-task">
+        Revenir à l'accueil
+      </Link>
     </section>
   );
 }
