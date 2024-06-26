2abef62812bfadf593de253659e050a4cc8a6481
SnakouZ <gillianlevert.dev@protonmail.com>
2024-06-26 14:43:24 +0200

    vercel deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+const defaultUrl = "https://taskmanager-self-gamma.vercel.app" ? "https://taskmanager-self-gamma.vercel.app" : "http://localhost:3000";
+
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
